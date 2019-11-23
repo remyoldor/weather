cc66ffa7b07bf465918f92166e3bfdfb4fe92189
Rodrigo Naranja <rodrigonaranja@gmail.com>
2019-11-23 15:18:02 -0300

    Inicio

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +10,43 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  public latitud: any = "";
+  public longitud: any = "";
+  public timestamp: any = "";
+
+  public ciudad: string = "";
+  public temperatura: string = "";
+
+  constructor(public platform: Platform, public geolocation: Geolocation, public httpClient: HttpClient) {
+
+    this.platform.ready().then(() =>{
+
+      this.getUbicacion();
+  
+      let watch = geolocation.watchPosition();
+      watch.subscribe((data) => {
+        // data can be a set of coordinates, or an error (if an error occurred).
+        // data.coords.latitud
+        // data.coords.longitud
+      });
+
+    });
+
+   }
+
+   getUbicacion (){
+     this.geolocation.getCurrentPosition().then((resp) => {
+       this.latitud = resp.coords.latitude;
+       this.longitud = resp.coords.longitude;
+       console.log('Mi latitud: ', resp.coords.latitude);
+       console.log('My longitud: ', resp.coords.longitude);
+     }).catch((error) => {
+       console.log('Error getting location', error);
+     });
+   }
+
+   getTemperatura(){
+     
+   }
 
 }
