3c64081d284a746bd5d3f2bf0928f75f337e4aec
Rodrigo Naranja <rodrigonaranja@gmail.com>
2019-11-24 23:53:02 -0300

    Mejora visual Tu ubicacion

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -11,47 +11,65 @@ import { HttpClient } from '@angular/common/http';
 export class Tab1Page {
 
   private appid = "d501e92ea8e61ca515352dfa01e4fc3d";
+  public loading: boolean = true;
 
-  public latitud    : any    = "";
-  public longitud   : any    = "";
+  public latitud    : any = ""   ;
+  public longitud   : any = ""   ;
   public ciudad     : string = "";
-  public icon       : any    = "";
+  public icon       : any = ""   ;
   public temperatura: string = "";
-  public timestamp  : any    = "";
-  public type       : any    = "";
+  public temp_max   : string = "";
+  public temp_min   : string = "";
+  public humedad    : number     ;
+  public descripcion: string = "";
+  public timestamp  : any = ""   ;
+  public type       : any = ""   ;
 
 
   constructor(public platform: Platform, public geolocation: Geolocation, public httpClient: HttpClient) {
 
-    this.platform.ready().then(() =>{
+    this.platform.ready().then(() => {
       this.getUbicacion();
     });
+    
+  }
+  
+  doRefresh(event) {
+    try {
+      this.getUbicacion();
+      event.target.complete();
+    } catch (error) {
+      console.log("Ha ocurrido un error al actualizar.")
+    }
+  }
+  
+  getUbicacion() {
+    this.geolocation.getCurrentPosition().then((resp) => {
+      this.latitud = resp.coords.latitude;
+      this.longitud = resp.coords.longitude;
+      this.getTemperatura(this.latitud, this.longitud);
+    }).catch((error) => {
+      console.log('Ha ocurrido un error obteniendo la ubicacion: ', error);
+    });
+  }
+  
+  getTemperatura(latitud, longitud) {
+    
+    var url = "https://api.openweathermap.org/data/2.5/weather?lat=" + latitud + "&lon=" + longitud + "&lang=es&appid=" + this.appid;
 
-   }
-
-   getUbicacion (){
-     this.geolocation.getCurrentPosition().then((resp) => {
-       this.latitud = resp.coords.latitude;
-       this.longitud = resp.coords.longitude;
-       this.getTemperatura(this.latitud, this.longitud);
-     }).catch((error) => {
-       console.log('Ha ocurrido un error obteniendo la ubicacion: ', error);
-     });
-   }
-
-   getTemperatura(latitud, longitud){
-
-     var url = "https://api.openweathermap.org/data/2.5/weather?lat=" + latitud + "&lon=" + longitud +"&lang=es&appid="+ this.appid;
-
-     this.httpClient.get(url).subscribe((data) => {
+    this.httpClient.get(url).subscribe((data) => {
       console.log(data);
       var obj = <any>data;
       this.ciudad = obj.name;
       this.type = obj.weather[0].main;
-      this.icon = "https://openweathermap.org/img/w/" + obj.weather[0].icon+".png";
-      this.temperatura = ((parseFloat(obj.main.temp)-273.15).toFixed(2)).toString()+" ºC";
-
-     })
-   }
+      // this.icon = "https://openweathermap.org/img/w/" + obj.weather[0].icon+".png";
+      this.icon = "/assets/img/png/" + obj.weather[0].icon.slice(0, 3) + ".png";
+      this.temperatura = ((parseFloat(obj.main.temp) - 273.15).toFixed(2)).toString().split(".")[0] + "º";
+      this.temp_max = ((parseFloat(obj.main.temp_max) - 273.15).toFixed(2)).toString().split(".")[0] + "º";
+      this.temp_min = ((parseFloat(obj.main.temp_min) - 273.15).toFixed(2)).toString().split(".")[0] + "º";
+      this.descripcion = obj.weather[0].description;
+      this.humedad = obj.main.humidity;
+    })
+  }
 
 }
